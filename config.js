f57bed32296ffff5d825ec904c5dada04d88350d
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-07-19 12:04:08 -0700

    config setup

---
+++ config.js
@@ -0,0 +1,19 @@
+/* Config */
+const twitchTvHandle = "bdougieYO";
+const repoOwner = "open-sauced";
+const repoName = "open-sauced";
+const PAUSE_DURATION = 30 * 1000; // 30 seconds
+const DISPLAY_DURATION = 10 * 1000; // 10 seconds
+
+/* DOM */
+const container = document.querySelector(".gif");
+const img = new Image();
+
+/* GIFs */
+const beyGif = "https://media.giphy.com/media/VxkNDa92gcsRq/giphy.gif";
+const welcomeGif = "https://media.giphy.com/media/l3V0doGbp2EDaLHJC/giphy.gif";
+const pizzaGif = "https://media.giphy.com/media/3o6nUXaNE4wdhq8Foc/giphy.gif";
+
+/* Sound Effects */
+const pewAudio = new Audio("horn.wav");
+const magicChime = new Audio("Magic_Chime.mp3");
