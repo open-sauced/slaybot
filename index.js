a950852c3c56f51315ff5638cf64035eabe85fe9
Brian Douglas <bdougie@users.noreply.github.com>
2020-07-19 12:05:16 -0700

    Queue (#1)
    * add queue
    
    * note
    
    * more re-orgs and remove the gifremove
    
    * name change
    
    * config setup

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/* Config */
-const twitchTvHandle = "bdougieYO";
-const repoOwner = "sgrove";
-const repoName = "blog";
+const queue = new Queue();
 
 /* OneGraph websocket subscriptions */
 const OneGraphSubscriptionPackage = window["onegraph-subscription-client"];
@@ -14,7 +11,7 @@ const ONEGRAPH_APP_ID = "e2ce0bcc-b5b6-42b7-a28e-3a6579d69ecd";
 const auth = new OneGraphAuth({
   appId: ONEGRAPH_APP_ID,
 });
-const client = new SubscriptionClient(ONEGRAPH_APP_ID, { oneGraphAuth: auth });
+const client = new SubscriptionClient(ONEGRAPH_APP_ID, {oneGraphAuth: auth});
 
 window.appId = ONEGRAPH_APP_ID;
 window.auth = auth;
@@ -50,79 +47,6 @@ const removeLoginButton = () => {
   }
 };
 
-/* Sound Effects */
-const pewAudio = new Audio("horn.wav");
-const magicChime = new Audio("Magic_Chime.mp3");
-
-/* GIFs */
-const beyGif = "https://media.giphy.com/media/VxkNDa92gcsRq/giphy.gif";
-const welcomeGif = "https://media.giphy.com/media/l3V0doGbp2EDaLHJC/giphy.gif";
-const pizzaGif = "https://media.giphy.com/media/3o6nUXaNE4wdhq8Foc/giphy.gif";
-
-ComfyJS.Init(twitchTvHandle);
-ComfyJS.onCommand = (user, command, message, flags, extra) => {
-  if (flags.broadcaster && command == "test") {
-    console.log("!test was typed in chat");
-  }
-
-  if (command == "yo") {
-    console.log("!yo was typed in chat");
-    new gifAlert(user, beyGif, pewAudio, command);
-    setTimeout(removeGif, 5000);
-  }
-
-  if (command == "welcome") {
-    console.log("!welcome was typed in the chat");
-    new gifAlert(message, welcomeGif, magicChime, command);
-    setTimeout(removeGif, 5000);
-  }
-
-  if (flags.broadcaster && command == "pizza") {
-    console.log("!pizza was typed in chat");
-    new gifAlert(message, pizzaGif, magicChime, command);
-    setTimeout(removeGif, 5000);
-  }
-};
-
-ComfyJS.onChat = (user, message, flags, self, extra) => {
-  console.log(user + ":", message);
-};
-
-const generateTitle = {
-  yo: " is hype!",
-  welcome: " needs a welcome!",
-  pizza: " needed a pizza party!",
-  starred: " starred us, like we knew they would!",
-};
-
-function gifAlert(user, gif, audio, type) {
-  removeGif(); // ensure that any previous gif is removed
-
-  const container = document.createElement("div");
-
-  const img = new Image();
-  img.src = gif;
-
-  const title = document.createElement("h1");
-  title.innerHTML = user + generateTitle[type];
-  title.classList.add("text-shadows")
-
-  const content = document.getElementById("content");
-  content.appendChild(title);
-
-  container.appendChild(img);
-  content.appendChild(container);
-  audio.play();
-  setTimeout(removeGif, 5000);
-}
-
-function removeGif() {
-  const contentElement = document.getElementById("content");
-  if (contentElement.firstElementChild) {
-    contentElement.firstElementChild.remove();
-  }
-}
-
 const startGitHubSubscription = async (auth, client, repoOwner, repoName) => {
   client
     .request({
@@ -153,7 +77,7 @@ const startGitHubSubscription = async (auth, client, repoOwner, repoName) => {
       operationName: "OnStarEvent",
     })
     .subscribe(
-      (next) => {
+      next => {
         const action = next.data.github.starEvent.action;
         const sender = next.data.github.starEvent.sender;
         const repo = next.data.github.starEvent.repository;
@@ -173,10 +97,9 @@ const startGitHubSubscription = async (auth, client, repoOwner, repoName) => {
         console.log(message);
 
         new gifAlert(login, pizzaGif, magicChime, "starred");
-        setTimeout(removeGif, 5000);
       },
-      (error) => console.error(error),
-      () => console.log("done")
+      error => console.error(error),
+      () => console.log("done"),
     );
 };
 
