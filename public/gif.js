3502620ad4e902bea17edb3ab8e3a304899487ea
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-04-13 15:53:20 -0400

    fix the bdougie gif

--- public/gif.js
+++ public/gif.js
@@ -19,7 +19,7 @@ const queue = new Queue();
 const beyGif = "https://media.giphy.com/media/VxkNDa92gcsRq/giphy.gif";
 const welcomeGif = "https://media.giphy.com/media/l3V0doGbp2EDaLHJC/giphy.gif";
 const pizzaGif = "https://media.giphy.com/media/3o6nUXaNE4wdhq8Foc/giphy.gif";
-const bdougie = "https://media.giphy.com/media/LT1Pq74cXuNQxyUmLk/giphy.gif";
+const bdougie = "/images/bdougie1.gif";
 
 /* Sound Effects */
 const pewAudio = new Audio("audio/horn.wav");
