2fff2de6e3b4a8b69b1364d3591dad165c91cfa1
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-04-13 16:04:16 -0400

    rename public to client

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ webhooks.onError((error) => {
 const app = express();
 
 // add middleware to server the static files
-app.use(express.static('public'))
+app.use(express.static('client'))
 
 // add middleware for the webhooks
 app.use(webhooks.middleware)
